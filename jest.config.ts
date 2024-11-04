5f4b0a92e033b31d49ccfeca5e5981094712cbd6
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2024-11-04 22:33:44 +0700

    setup workspace using turbo

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/'
+  ],
 };
 
 export default config;
