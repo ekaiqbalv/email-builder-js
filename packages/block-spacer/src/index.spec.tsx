b7732e37470aa92bf8c38d818d3d7746bfa80123
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2024-11-05 16:36:48 +0700

    add responsive heights for block spacer

--- packages/block-spacer/src/index.spec.tsx
+++ packages/block-spacer/src/index.spec.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import { matchers } from '@emotion/jest';
 import { render } from '@testing-library/react';
+import { BREAKPOINTS } from '@usewaypoint/shared';
 
 import { Spacer } from '.';
+
+expect.extend(matchers);
 
 describe('Spacer', () => {
   it('renders with default values', () => {
@@ -11,5 +15,19 @@ describe('Spacer', () => {
 
   it('renders with props', () => {
     expect(render(<Spacer props={{ height: 10 }} />).asFragment()).toMatchSnapshot();
+  });
+
+  it('applies responsive heights correctly', () => {
+    const { container } = render(<Spacer props={{ height: 20 }} />);
+    expect(container.firstChild).toHaveStyleRule('height', '16px');
+    expect(container.firstChild).toHaveStyleRule('height', '17px!important', {
+      media: `(min-width: ${BREAKPOINTS.sm}px)`,
+    });
+    expect(container.firstChild).toHaveStyleRule('height', '19px!important', {
+      media: `(min-width: ${BREAKPOINTS.md}px)`,
+    });
+    expect(container.firstChild).toHaveStyleRule('height', '20px!important', {
+      media: `(min-width: ${BREAKPOINTS.lg}px)`,
+    });
   });
 });
--- packages/block-spacer/src/index.tsx
+++ packages/block-spacer/src/index.tsx
@@ -1,5 +1,10 @@
-import React, { CSSProperties } from 'react';
+/** @jsxRuntime automatic */
+/** @jsxImportSource @emotion/react */
+import { useMemo } from 'react';
 import { z } from 'zod';
+
+import { css } from '@emotion/react';
+import { generateResponsiveStyles } from '@usewaypoint/shared';
 
 export const SpacerPropsSchema = z.object({
   props: z
@@ -16,9 +21,34 @@ export const SpacerPropsDefaults = {
   height: 16,
 };
 
+const SIZE_RATIOS = {
+  xs: 0.8,
+  sm: 0.85,
+  md: 0.95,
+  lg: 1,
+};
+
+const calculateResponsiveHeight = (desktopSize: number) => ({
+  xs: `${Math.round(desktopSize * SIZE_RATIOS.xs)}px`,
+  sm: `${Math.round(desktopSize * SIZE_RATIOS.sm)}px`,
+  md: `${Math.round(desktopSize * SIZE_RATIOS.md)}px`,
+  lg: `${desktopSize}px`,
+});
+
 export function Spacer({ props }: SpacerProps) {
-  const style: CSSProperties = {
-    height: props?.height ?? SpacerPropsDefaults.height,
-  };
-  return <div style={style} />;
+  const height = props?.height ?? SpacerPropsDefaults.height;
+  const responsiveHeights = useMemo(() => calculateResponsiveHeight(height), [height]);
+  const heightCss = useMemo(
+    () => css`
+      ${generateResponsiveStyles({
+        xs: { height: responsiveHeights.xs },
+        sm: { height: responsiveHeights.sm },
+        md: { height: responsiveHeights.md },
+        lg: { height: responsiveHeights.lg },
+      })}
+    `,
+    [responsiveHeights]
+  );
+
+  return <div css={heightCss} />;
 }
